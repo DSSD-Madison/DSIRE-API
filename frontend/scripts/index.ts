0f665d482619380afc74114aeeec24168b048f53
eaberger2 <eaberger2@wisc.edu>
2023-02-15 20:09:24 -0600

    beginning of frontend

--- frontend/scripts/index.ts
+++ frontend/scripts/index.ts
@@ -1,1 +1,17 @@
-console.log("hello, world")
+async function submitForm(){
+    let username = document.forms["signupform"]["username"].value;
+    let email = document.forms["signuptform"]["email"].value;
+    let org = document.forms["signupform"]["email"].value;
+    console.log(username);
+    const response = await fetch('https://jte4x8sz31.execute-api.us-east-1.amazonaws.com/testingStage',{
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            org: org,
+        })
+    })
+}
