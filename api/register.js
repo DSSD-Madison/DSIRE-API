8906b6d689be51e55c61062f130c924450a4e124
Lyn Murph Talley (they/them) <lynmurph.talley@wisc.edu>
2023-02-28 18:13:51 -0600

    deleting misnamed/duplicate files

--- api/register.js
+++ api/register.js
@@ -1,17 +1,16 @@
-export default (token) => `
-<!DOCTYPE html>
-<html>
-	<body>
-		Click here to verify your email for the DSIRE API.
-		<button onclick=redirectTo(this) value="https://dsireusa.org/register/verify?token=${token}">Verify email</button>
-		<script>
-		const redirectTo(link) {
-			location.href(link.value);
-		}
-		</script>
-		If you did not make this request, please ignore this email. No further action is required.
-		
-		Thanks for your interest in DSIRE!
-	</body>
-</html>			
-`	
+export default (token) => {`
+	<html>
+		<div id="button" style="width:20px; height:20px"></div>
+		<body>					
+			<p style="font-family:arial">Click here to verify your email for the DSIRE API.</p>
+			<button style="border-radius:4px; border: 3px solid #c00" onclick="redirectTo(this)" value="https://www.dsireusa.org/register/verify?token=${token}">Verify email</button> <br>						
+			<script>
+			function redirectTo(link) { 
+				console.log(link);
+				location.href = link.value;
+			}
+			</script>
+			<p style="font-family:arial">If you didn't make this request, please ignore this email. No further action is required.<p>
+		</body>
+	</html>
+`}
