22a19f8738426788fe06c48c15e8457011ea9cd0
eaberger2 <eaberger2@wisc.edu>
2023-04-25 00:45:07 -0500

    not much progress but pushing for nic!

--- api/src/handlers/rest.ts
+++ api/src/handlers/rest.ts
@@ -1,14 +1,30 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
 import {graphql as GraphQL} from "graphql"
 import {PrismaClient} from "@prisma/client"
+import {z} from 'zod';
 
 import {CORS_HEADERS} from "./headers"
 import schema from "./graphql/schema"
 
 export default async function rest(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
 
+    const data = z.object({
+        states: z.string().optional(),
+        category: z.string().optional(),
+        pageSize: z.string().optional(),
+        page: z.string().optional()
+    });
+
+    const parsed_data = (inputs: unknown) => {
+        const parsed = data.parse(event.queryStringParameters);
+        return parsed;
+    }
+
+    const params = event.queryStringParameters
+
+    //const formData = event.safeParse(typeof(event.queryStringParameters) === "string" ? JSON.parse(event.queryStringParameters) : event.queryStringParameters);
     //const {query, operationName, variableValues} = JSON.parse(event.body as any)
-    const params = JSON.parse(event.queryStringParameters as any)
+    
 
     return {
         headers: {
