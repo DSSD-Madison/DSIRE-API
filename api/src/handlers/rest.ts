94ce01edf86badfc0f72c7657a28da8cc9aa87bc
eaberger2 <eaberger2@wisc.edu>
2023-04-24 22:52:00 -0500

    check response

---
+++ api/src/handlers/rest.ts
@@ -0,0 +1,37 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
+import {graphql as GraphQL} from "graphql"
+import {PrismaClient} from "@prisma/client"
+
+import {CORS_HEADERS} from "./headers"
+import schema from "./graphql/schema"
+
+export default async function rest(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+
+    const {query, operationName, variableValues} = JSON.parse(event.body as any)
+
+    return {
+        headers: {
+            ...CORS_HEADERS,
+            "content-type": "application/json"
+        },
+        statusCode: 200,
+        body: JSON.stringify(query,operationName,variableValues)
+    }
+
+    /*return {
+        headers: {
+            ...CORS_HEADERS,
+            "content-type": "application/json"
+        },
+        statusCode: 200,
+        body: JSON.stringify(await GraphQL({
+            schema: schema,
+            contextValue: {
+                prisma: new PrismaClient()
+            },
+            source: query,
+            operationName,
+            variableValues
+        }))
+    }*/
+}
